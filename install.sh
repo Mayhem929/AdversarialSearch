bf2433cc043a77a3c5d02020745ae9ee7c1f2b8f
Guillermo <guillermogtrenado@gmail.com>
2023-05-10 15:37:10 +0200

    Optional Doxygen generation

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+BUILD_DOC=OFF # ON or OFF
+
 # Install SFML, if not Ubuntu, comment next line & check https://www.sfml-dev.org/tutorials/2.5/start-linux.php
-sudo apt-get install libsfml-dev
+sudo apt-get -y install libsfml-dev
+# For doxygen generation
+if [ "$BUILD_DOC" = "ON" ]; then
+    sudo apt-get -y install doxygen graphviz
+fi
 
 mkdir build
 cd build
-cmake -DCMAKE_BUILD_TYPE=Debug ..
+cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_DOC=$BUILD_DOC .. 
 make -j8
 cd ..
 
