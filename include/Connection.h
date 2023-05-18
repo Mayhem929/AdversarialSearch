920966c93b8365ba9358dd931c86238a11360b8b
Juan Luis <jlsuarezdiaz@ugr.es>
2023-05-18 21:43:45 +0200

    Ninja update

--- include/Connection.h
+++ include/Connection.h
@@ -13,8 +13,8 @@ using namespace sf;
 using namespace std;
 
 // Para control de versiones y obligar a actualizar a versiones incompatibles con el online.
-# define NINJA_VERSION 1
-# define ONLINE_VERSION 2
+# define NINJA_VERSION 2
+# define ONLINE_VERSION 3
 
 enum MessageKind{
     NOP = 0,
